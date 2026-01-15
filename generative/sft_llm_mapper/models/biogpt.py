5bbda7200b656aaea22f49b6903edcd216863226
ilan <ilan.bacry20@gmail.com>
2026-01-15 16:57:40 +0100

    add sft

--- generative/sft_llm_mapper/models/biogpt.py
+++ generative/sft_llm_mapper/models/biogpt.py
@@ -19,7 +19,7 @@ def load_biogpt(
 
     llm = AutoModelForCausalLM.from_pretrained(
         model_name,
-        torch_dtype=torch.bfloat16 if torch.cuda.is_available() else torch.float32,
+        torch_dtype= torch.float32
     ).to(device)
 
     for p in llm.parameters():
