40b100248f5886b8d6f026a64c8f519357487432
ilan <ilan.bacry20@gmail.com>
2026-01-15 14:13:39 +0100

    add path checkpoint

--- generative/llm-frozen-approach/training/train_encoder_mapper.py
+++ generative/llm-frozen-approach/training/train_encoder_mapper.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 import sys
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parents[3]
+ROOT = Path(__file__).resolve().parents[2]
 sys.path.append(str(ROOT))
 
 from data_utils import PreprocessedGraphDataset, x_map, e_map
