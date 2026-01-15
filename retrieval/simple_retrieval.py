7224374b69ba2cd27b47dec7385b3022d9aba4dc
ilan <ilan.bacry20@gmail.com>
2026-01-15 10:24:33 +0100

    add notebook train gps

--- retrieval/simple_retrieval.py
+++ retrieval/simple_retrieval.py
@@ -110,7 +110,7 @@ def main():
 
     train_emb = load_id2emb(args.train_emb)
     emb_dim = len(next(iter(train_emb.values())))
-    
+
     print(f"Loading model from {args.model_path}")
     if args.encoder == "gine":
         model = MolGNN(out_dim=emb_dim).to(device)
@@ -118,10 +118,10 @@ def main():
         model.eval()
 
     elif args.encoder == "gps":
-        model = load_graph_encoder_from_checkpoint(
-            model_path=args.model_path,
-            device=device,
-        )
+        cfg = GraphEncoderConfig(out_dim=emb_dim)
+        model = GraphEncoder(cfg).to(device)
+        model.load_state_dict(torch.load(args.model_path, map_location=device))
+        model.eval()
     
     else:
         raise ValueError(f"Unknown encoder {args.encoder}")
