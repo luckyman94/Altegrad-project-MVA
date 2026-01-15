7f168a7243fbbd4cdab1a8d0bc3bb0a046d75ddc
ilan <ilan.bacry20@gmail.com>
2026-01-15 09:33:18 +0100

    fix

--- retrieval/gine_encoder/train.py
+++ retrieval/gine_encoder/train.py
@@ -7,7 +7,7 @@ import sys
 from pathlib import Path
 
 ROOT = Path(__file__).resolve().parents[2]
-sys.path.append(0, str(ROOT))
+sys.path.append(str(ROOT))
 from losses.infonce import infonce_loss
 from data_utils import PreprocessedGraphDataset, collate_fn, load_id2emb
 from encoder import MolGNN
